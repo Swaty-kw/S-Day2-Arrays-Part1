9764943bf55076a0978914d3f3cf70c2ed53ff13
Swaty-kw <dashtilimited@gmail.com>
2024-08-05 20:22:07 +0300

    done with array

---
+++ arrays1.js
@@ -0,0 +1,9 @@
+let groceries = ["milk", "cheese", "bread", "water", "lemon", "potato"];
+console.log(groceries[1]);
+let numberofgroceries = groceries.length;
+console.log(groceries[5]);
+groceries.pop();
+console.log(groceries);
+groceries.push("Tona", "cat treats");
+let firstThreeItems = groceries.slice(0, 4);
+console.log(firstThreeItems);
